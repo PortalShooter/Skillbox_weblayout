0a81a866a4ed9f7c47f098f93167bd08e308b663
PortalShooter <70528020+PortalShooter@users.noreply.github.com>
2021-02-13 18:35:51 +0300

    1

--- 16_knowledge-base/js.js
+++ 16_knowledge-base/js.js
@@ -1,2 +1,31 @@
 const element = document.querySelector('.select');
-const choices = new Choices(element);
+const choices = new Choices(element,{searchEnabled: false,itemSelectText: '',shouldSort: false});
+
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init(){
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [48.87219657376512,2.354223999999991],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 12
+    });
+    // Создание геообъекта с типом точка (метка).
+    var myPlacemark = new ymaps.Placemark([48.87219657376512,2.354223999999991], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: '/img/Map.svg',
+      iconImageSize: [28, 40],
+      iconImageOffset: [-14, -40]
+  });
+
+// Размещение геообъекта на карте.
+myMap.geoObjects.add(myPlacemark);
+}
+
